cd9b63187db73a0ba659c6e4c037693b6d650231
Calla Rudolph <callarudolph@gmail.com>
2017-06-26 21:02:19 -0700

    dimensions and display them on /view_parcel

--- app/app.php
+++ app/app.php
@@ -1,12 +1,55 @@
 <?php
     date_default_timezone_set('America/Los_Angeles');
     require_once __DIR__."/../vendor/autoload.php";
+    require_once __DIR__."/../src/parcel.php";
 
     $app = new Silex\Application();
 
     $app->get("/", function() {
-        return "Hello friend!";
+        return "
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <link rel='stylesheet' href=href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css'>
+            <title>Send yr package</title>
+        </head>
+        <body>
+            <div class='container'>
+                <h1>Parcel Info</h1>
+                <p>Enter the dimensions of your package to see the specs:</p>
+                <form action='/view_parcel'>
+                    <div class='form-group'>
+                        <label for='weight'>Enter the weight:</label>
+                        <input id='weight' name='weight' class='form-control' type='number'>
+                    </div>
+                    <div class='form-group'>
+                        <label for='height'>Enter the height:</label>
+                        <input id='height' name='height' class='form-control' type='number'>
+                    </div>
+                    <div class='form-group'>
+                        <label for='length'>Enter the length:</label>
+                        <input id='length' name='length' class='form-control' type='number'>
+                    </div>
+                    <div class='form-group'>
+                        <label for='width'>Enter the width:</label>
+                        <input id='width' name='width' class='form-control' type='number'>
+                    </div>
+                    <button type='submit' class='btn-danger'>Check it</button>
+                </form>
+            </div>
+        </body>
+        </html>
+        ";
     });
 
+    $app->get("/view_parcel", function() {
+        $my_parcel = new Parcel($_GET['weight'], $_GET['height'], $_GET['length'], $_GET['width']);
+        return "<h2>Your info:</h2>
+        <p>The weight entered was: " . $my_parcel->getWeight() . " lbs.</p>
+        <p>The height entered was: " . $my_parcel->getHeight() . " inches</p>
+        <p>The length entered was: " . $my_parcel->getLength() . " inches</p>
+        <p>The width entered was: " . $my_parcel->getWidth() . " inches</p>";
+    });
     return $app;
+
 ?>
